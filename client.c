3fc57d2c7d389675c304376fe2c5b05366a3e82e
DoctorB <150272448+DoctorB-CA@users.noreply.github.com>
2024-08-12 15:50:03 +0300

    addint command to server + fixing some bugs
    adding to the server command, so now it more easy to control stuff.
    
    but there is a problem i cant fix, this is the multicast behave like broadcast

--- client.c
+++ client.c
@@ -8,11 +8,9 @@
 #include <pthread.h>
 
 #define BUFFER_SIZE 1024
-#define maxLen 256
-#define password "Kofiko"
-#define MAX_SALES 10
 #define PORT 8083
 #define IP "127.0.0.1"
+#define MAX_SALES 10
 
 // Prototype declarations
 void getting_data(int sock, char buffer4[BUFFER_SIZE]);
@@ -28,10 +26,9 @@ int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
     char buffer[BUFFER_SIZE] = {0};
-    char buffer2[BUFFER_SIZE] = {0};
-    char buffer3[BUFFER_SIZE] = {0};    
-    char buffer4[BUFFER_SIZE] = {0};
     char num_menu[BUFFER_SIZE];  // Changed to BUFFER_SIZE
+    multicast_info m_info;
+    pthread_t multicast_thread;
 
     // Create socket
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -89,10 +86,10 @@ int main() {
     memset(buffer, 0, BUFFER_SIZE);
 
     // Format data to send to the server
-    snprintf(buffer2, BUFFER_SIZE, "%s", pass);
+    snprintf(buffer, BUFFER_SIZE, "%s", pass);
 
     // Send data to the server
-    ssize_t pass_sent = send(sock, buffer2, strlen(buffer2), 0);
+    ssize_t pass_sent = send(sock, buffer, strlen(buffer), 0);
     if (pass_sent < 0) {
         perror("send failed");
         close(sock);
@@ -101,106 +98,81 @@ int main() {
 
     printf("Connecting to the server... Data sent: user %s, pass %s\n", user, pass);
 
-    // Main loop to check for user input or other operations
-    printf("Loading\n");
+    // Wait for server's authentication response
+    getting_data(sock, buffer);
+    printf("Server response: %s\n", buffer);
 
-    // Wait for the server's response after sending the password
-    getting_data(sock, buffer4);
+    // Main loop to handle menu selection
+    while (1) {
+        getting_data(sock, buffer);
+        printf("%s", buffer);
 
-    // Check the server's response
-    if (strstr(buffer4, "wrong password") != NULL) {
-        printf("Server response: %s\n", buffer4);
-        close(sock);
-        return 0;  // Exit the program if the password is wrong
-    } else if (strstr(buffer4, "Authentication successful") != NULL) {
-        printf("Server response: %s\n", buffer4);
-    }
-
-    while (1) {            // LOOP TO MENU STAGE
-        getting_data(sock, buffer4);
-        // Clear buffer to ensure no garbage data is sent
-        int buffer_numb = atoi(buffer4);
-        memset(buffer4, 0, BUFFER_SIZE);
-        if (buffer_numb == 371) {
-            printf("the server accept your request to join the sale\n");
-            break;
-        } else if (buffer_numb == -1) {
-            printf("the server accept you request to leave\n");
-            break;
-        }
-
-        printf("Choose menu number ");
+        printf("Choose a sale number: ");
         scanf("%49s", num_menu);  // Changed to BUFFER_SIZE
 
-        // Clear buffer to ensure no garbage data is sent
-        memset(buffer4, 0, BUFFER_SIZE);
-
-        // Format data to send to the server
-        snprintf(buffer4, BUFFER_SIZE, "%s", num_menu); 
-        // Send data to the server
+        // Send the chosen menu number to the server
         sending_data(sock, num_menu);
 
-        // Get multicast IP from the server
-        getting_data(sock, buffer4);
-        printf("Multicast IP received from the server: %s\n", buffer4);
+        // Receive multicast IP from the server
+        getting_data(sock, buffer);
+        printf("Multicast IP received from the server: %s\n", buffer);
 
         // Extract multicast IP from the received data
-        char multicast_ip[50];
-        sscanf(buffer4, "Multicast IP: %49s", multicast_ip);
-
-        // Create a thread to receive multicast messages
-        multicast_info m_info;
-        strncpy(m_info.multicast_ip, multicast_ip, 50);
+        sscanf(buffer, "Multicast IP: %49s", m_info.multicast_ip);
         m_info.multicast_port = 12345;
 
-        pthread_t multicast_thread;
+        // Create a thread to receive multicast messages
         if (pthread_create(&multicast_thread, NULL, receive_multicast, &m_info) != 0) {
             perror("Failed to create multicast receiver thread");
         }
         pthread_detach(multicast_thread);  // Automatically reclaim resources when the thread terminates
+        break;  // Exit after setting up the multicast listener
     }
 
-    while (1) {            // LOOP FOR SALE
-        // sending_data(sock, num_menu);        
-        // getting_data(sock, buffer4);
+    // The main loop keeps the connection open
+    while (1) {
+        // Implement additional logic here, such as receiving specific commands or sending more data
+        // For now, just keep the connection open
+        sleep(1);
     }
 
-    // Close the socket
+    // The connection will only close when the user decides to exit the loop
     close(sock);
     printf("Connection closed.\n");
 
     return 0;
 }
 
-void getting_data(int sock, char buffer4[BUFFER_SIZE]) {
-    int data_recieved = recv(sock, buffer4, BUFFER_SIZE, 0);    // getting menu numb
-    if (data_recieved > 0) 
-        printf("this data delivered from the server:\n%s\n", buffer4);
-    else
-        printf("Bad getting data\n");
+void getting_data(int sock, char buffer[BUFFER_SIZE]) {
+    int data_received = recv(sock, buffer, BUFFER_SIZE, 0);
+    if (data_received > 0) {
+        buffer[data_received] = '\0';
+    } else {
+        printf("Failed to receive data.\n");
+    }
 }
 
-void sending_data(int sock, char buffer4[BUFFER_SIZE]) {
-    int send_me = send(sock, buffer4, strlen(buffer4), 0);
-    if (send_me < 0) 
-        perror("send failed");
-    printf("sss");
+void sending_data(int sock, char buffer[BUFFER_SIZE]) {
+    if (send(sock, buffer, strlen(buffer), 0) < 0) {
+        perror("Failed to send data");
+    }
 }
 
 void *receive_multicast(void *arg) {
     multicast_info *m_info = (multicast_info *)arg;
     int sockfd;
-    struct sockaddr_in multicast_addr;
+    struct sockaddr_in local_addr;
     struct ip_mreq mreq;
     char buffer[BUFFER_SIZE];
 
+    // Create a UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
         perror("socket creation failed");
         pthread_exit(NULL);
     }
 
-    // Allow multiple sockets to use the same PORT number
+    // Allow multiple sockets to use the same port number
     unsigned int yes = 1;
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
         perror("Reusing ADDR failed");
@@ -208,26 +180,29 @@ void *receive_multicast(void *arg) {
         pthread_exit(NULL);
     }
 
-    memset(&multicast_addr, 0, sizeof(multicast_addr));
-    multicast_addr.sin_family = AF_INET;
-    multicast_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    multicast_addr.sin_port = htons(m_info->multicast_port);
+    // Set up the local address structure
+    memset(&local_addr, 0, sizeof(local_addr));
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = htonl(INADDR_ANY); // Listen on any local address
+    local_addr.sin_port = htons(m_info->multicast_port);
 
-    if (bind(sockfd, (struct sockaddr *)&multicast_addr, sizeof(multicast_addr)) < 0) {
+    // Bind the socket to the local address and port
+    if (bind(sockfd, (struct sockaddr *)&local_addr, sizeof(local_addr)) < 0) {
         perror("bind failed");
         close(sockfd);
         pthread_exit(NULL);
     }
 
+    // Join the multicast group
     mreq.imr_multiaddr.s_addr = inet_addr(m_info->multicast_ip);
-    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY);  // Use the default interface
     if (setsockopt(sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
         perror("setsockopt failed");
         close(sockfd);
         pthread_exit(NULL);
     }
 
+    // Receive messages from the multicast group
     while (1) {
         int nbytes = recv(sockfd, buffer, BUFFER_SIZE, 0);
         if (nbytes < 0) {
